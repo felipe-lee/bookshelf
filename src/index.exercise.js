e359f801d17c34caf1377ceee11c2a2dbdcf68b3
Felipe Lee <felipe@truss.works>
2022-03-10 19:24:23 -0800

    Add `Dialog` component for log in and register buttons

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Dialog } from "@reach/dialog"
+import "@reach/dialog/styles.css";
 
 import {Logo} from 'components/logo'
 
+const LOG_IN = "log in"
+const REGISTER = "register"
+const NONE = "none"
+
 const App = () => {
+  const [openModal, setOpenModal] = React.useState(NONE)
+
+  const close = () => {setOpenModal(NONE)}
+
   return (
       <>
         <Logo height={80} width={80}/>
@@ -11,11 +21,21 @@ const App = () => {
         <h1>Bookshelf</h1>
 
         <div>
-          <button onClick={() => alert('Log In button clicked')}>Log In</button>
+          <button onClick={() => setOpenModal(LOG_IN)}>Log In</button>
+
+          <Dialog isOpen={openModal === LOG_IN} onDismiss={close}>
+            <button className="close-button" onClick={close}>Close</button>
+            <h3>Log In</h3>
+          </Dialog>
         </div>
 
         <div>
-          <button onClick={() => alert('Register button clicked')}>Register</button>
+          <button onClick={() => setOpenModal(REGISTER)}>Register</button>
+
+          <Dialog aria-label="Log In Form" isOpen={openModal === REGISTER} onDismiss={close}>
+            <button className="close-button" onClick={close}>Close</button>
+            <h3>Register</h3>
+          </Dialog>
         </div>
       </>
   )
