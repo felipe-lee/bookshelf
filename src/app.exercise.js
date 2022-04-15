faa18fd0f889e5b1c74d1a1f44f5e8798c5e45e7
Felipe Lee <felipe@truss.works>
2022-04-14 19:12:54 -0700

    Add react-router to app

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -1,18 +1,15 @@
 /** @jsx jsx */
 import {jsx} from '@emotion/core'
-
 import * as React from 'react'
+import {BrowserRouter as Router} from 'react-router-dom'
+
 import * as auth from 'auth-provider'
-// React Router DOM exposes a context provider that all the React Router
-// components use to implicitly access the router data. We need to wrap our
-// AuthenticatedApp in the router
-// 🐨 import the BrowserRouter from 'react-router-dom'
-import {FullPageSpinner} from './components/lib'
-import * as colors from './styles/colors'
-import {client} from './utils/api-client'
-import {useAsync} from './utils/hooks'
-import {AuthenticatedApp} from './authenticated-app'
-import {UnauthenticatedApp} from './unauthenticated-app'
+import {AuthenticatedApp} from 'authenticated-app.exercise'
+import {FullPageSpinner} from 'components/lib'
+import * as colors from 'styles/colors'
+import {UnauthenticatedApp} from 'unauthenticated-app'
+import {client} from 'utils/api-client'
+import {useAsync} from 'utils/hooks'
 
 async function getUser() {
   let user = null
@@ -73,9 +70,10 @@ function App() {
 
   if (isSuccess) {
     const props = {user, login, register, logout}
-    // 🐨 wrap the BrowserRouter around the AuthenticatedApp
     return user ? (
-      <AuthenticatedApp {...props} />
+      <Router>
+        <AuthenticatedApp {...props} />
+      </Router>
     ) : (
       <UnauthenticatedApp {...props} />
     )
