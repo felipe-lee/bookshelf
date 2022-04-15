dc236ba5f2074a6404274c19428df26ae88c2918
Felipe Lee <felipe@truss.works>
2022-04-14 17:34:43 -0700

    Load user's data on page load if we have a token for them

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -6,9 +6,30 @@ import * as React from 'react'
 import * as auth from 'auth-provider'
 import {AuthenticatedApp} from 'authenticated-app'
 import {UnauthenticatedApp} from 'unauthenticated-app'
+import {client} from 'utils/api-client'
+
+const UNAUTHENTICATED_USER = null
+
+const checkForLoggedInUser = async () => {
+  let user = UNAUTHENTICATED_USER
+
+  const token = await auth.getToken()
+
+  if (token) {
+    await client('me', {token}).then(data => {
+      user = data.user
+    })
+  }
+
+  return user
+}
 
 function App() {
-  const [user, setUser] = React.useState(null)
+  const [user, setUser] = React.useState(UNAUTHENTICATED_USER)
+
+  React.useEffect(() => {
+    checkForLoggedInUser().then(u => setUser(u))
+  }, [])
 
   const login = form => auth.login(form).then(u => setUser(u))
 
@@ -16,7 +37,7 @@ function App() {
 
   const logout = () => {
     auth.logout()
-    setUser(null)
+    setUser(UNAUTHENTICATED_USER)
   }
 
   return user ? (
