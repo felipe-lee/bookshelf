a0a6fdbfc50cd23ebe199047732ce54c5b10c1ac
Felipe Lee <felipe@truss.works>
2022-04-15 12:34:48 -0700

    Add css to let user see when they are already on a page in the nav bar

--- src/authenticated-app.exercise.js
+++ src/authenticated-app.exercise.js
@@ -2,7 +2,7 @@
 import {jsx} from '@emotion/core'
 
 import * as React from 'react'
-import {Routes, Route, Link} from 'react-router-dom'
+import {Routes, Route, Link, useMatch} from 'react-router-dom'
 
 import {Button} from 'components/lib'
 import {BookScreen} from 'screens/book'
@@ -56,26 +56,37 @@ function AuthenticatedApp({user, logout}) {
 }
 
 function NavLink(props) {
-  return (
-    <Link
-      css={{
-        display: 'block',
-        padding: '8px 15px 8px 10px',
-        margin: '5px 0',
-        width: '100%',
-        height: '100%',
-        color: colors.text,
-        borderRadius: '2px',
-        borderLeft: '5px solid transparent',
-        ':hover': {
-          color: colors.indigo,
-          textDecoration: 'none',
-          background: colors.gray10,
-        },
-      }}
-      {...props}
-    />
-  )
+  const matches = useMatch(props.to)
+
+  const css = [
+    {
+      display: 'block',
+      padding: '8px 15px 8px 10px',
+      margin: '5px 0',
+      width: '100%',
+      height: '100%',
+      color: colors.text,
+      borderRadius: '2px',
+      borderLeft: '5px solid transparent',
+      ':hover': {
+        color: colors.indigo,
+        textDecoration: 'none',
+        background: colors.gray10,
+      },
+    },
+  ]
+
+  if (matches) {
+    css.push({
+      borderLeft: `5px solid ${colors.indigo}`,
+      background: colors.gray10,
+      ':hover': {
+        background: colors.gray20,
+      },
+    })
+  }
+
+  return <Link css={css} {...props} />
 }
 
 function Nav() {
