34807e8c5532487162423f4a3eb2dc1b952455e7
Felipe Lee <felipe@truss.works>
2022-06-16 17:12:41 -0700

    Add react query config to use custom settings for queries
    In particular, we want it to use our error boundary on errors, stop refetching on window focus, and only retry if the error isn't a 404 and there haven't been more than 2 errors

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,9 +1,25 @@
+import {ReactQueryConfigProvider} from 'react-query'
 import {loadDevTools} from './dev-tools/load'
 import './bootstrap'
 import * as React from 'react'
 import ReactDOM from 'react-dom'
 import {App} from './app'
 
+const queryConfig = {
+  queries: {
+    refetchOnWindowFocus: false,
+    retry(failureCount, error) {
+      return error.status !== 404 && failureCount <= 2
+    },
+    useErrorBoundary: true,
+  },
+}
+
 loadDevTools(() => {
-  ReactDOM.render(<App />, document.getElementById('root'))
+  ReactDOM.render(
+    <ReactQueryConfigProvider config={queryConfig}>
+      <App />
+    </ReactQueryConfigProvider>,
+    document.getElementById('root'),
+  )
 })
