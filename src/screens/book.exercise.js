02ba10b93aff22dd59ce0f2c7f00258bcbb5ce22
Felipe Lee <felipe@truss.works>
2022-06-16 17:42:56 -0700

    Add a spinner to indicate when notes are saving

--- src/screens/book.exercise.js
+++ src/screens/book.exercise.js
@@ -7,7 +7,7 @@ import * as React from 'react'
 import {FaRegCalendarAlt} from 'react-icons/fa'
 import {useParams} from 'react-router-dom'
 
-import {ErrorMessage, Textarea} from 'components/lib'
+import {ErrorMessage, Spinner, Textarea} from 'components/lib'
 import {Rating} from 'components/rating'
 import {StatusButtons} from 'components/status-buttons'
 import * as colors from 'styles/colors'
@@ -105,7 +105,7 @@ function ListItemTimeframe({listItem}) {
 }
 
 function NotesTextarea({listItem, user}) {
-  const [mutate, {error, isError}] = useUpdateListItem(user)
+  const [mutate, {error, isError, isLoading}] = useUpdateListItem(user)
 
   const debouncedMutate = React.useMemo(
     () => debounceFn(mutate, {wait: 300}),
@@ -138,6 +138,7 @@ function NotesTextarea({listItem, user}) {
             css={{marginLeft: 6, fontSize: '0.7em'}}
           />
         ) : null}
+        {isLoading ? <Spinner /> : null}
       </div>
       <Textarea
         id="notes"
