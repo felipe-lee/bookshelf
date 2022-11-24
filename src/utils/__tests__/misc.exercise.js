ee51c313ebb71ed5d5684c8c7fd612bd31cddc5a
Felipe Lee <felipe@truss.works>
2022-11-23 16:54:16 -0800

    Add test for `formatDate` function

--- src/utils/__tests__/misc.exercise.js
+++ src/utils/__tests__/misc.exercise.js
@@ -1,2 +1,7 @@
-test.todo('formatDate formats the date to look nice')
+import {formatDate} from 'utils/misc'
 
+test('formatDate formats the date to look nice', () => {
+  const date = new Date('November 23, 2022')
+
+  expect(formatDate(date)).toBe('Nov 22')
+})
